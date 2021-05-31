83ef3de8a1be7c1ba426c0e7adf4a03da9a831f7
Isaacmart <65898932+Isaacmart@users.noreply.github.com>
2021-05-31 13:54:36 -0500

    new

--- webhook.php
+++ webhook.php
@@ -11,26 +11,15 @@
     <div id="$data"></div>
     <?php
     
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // fetch RAW input
     $json = file_get_contents('php://input');
-
     // decode json
     $object = json_decode($json);
-
     // expecting valid json
     if (json_last_error() !== JSON_ERROR_NONE) {
         die(header('HTTP/1.0 415 Unsupported Media Type'));
     }
+    echo $post;
 
-    /**
-     * Do something with object, structure will be like:
-     * $object->accountId
-     * $object->details->items[0]['contactName']
-     */
-    // dump to file so you can see
-    file_put_contents('callback.test.txt', print_r($object, true));
-    }
     ?>
 
 
