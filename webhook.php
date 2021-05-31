3c37a742a44ffea63e7468d7b0f5e6676de12e09
Isaacmart <65898932+Isaacmart@users.noreply.github.com>
2021-05-31 03:32:48 -0500

    new webhook

--- webhook.php
+++ webhook.php
@@ -8,14 +8,9 @@
 <body>
     <h1>Receive Webhooks</h1>
 
-    <div id="webhook"></div>
-
     <?php 
-    
     $new_json = file_get_contents('php://input');
     $data =  json_decode('$new_json');
-    echo $data;
-
     ?>
 
 </body>
