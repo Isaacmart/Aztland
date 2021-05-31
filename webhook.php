c0e8dfed616e3f97d17f7f59580d0092fc8b7fba
Isaacmart <65898932+Isaacmart@users.noreply.github.com>
2021-05-31 04:06:28 -0500

    hew

--- webhook.php
+++ webhook.php
@@ -12,7 +12,7 @@
     <?php 
     $new_json = file_get_contents('php://input');
     $data =  json_decode('$new_json');
-    echo "<h2" . $data . "</h2";
+    echo "<h2>" . $data . "</h2>";
     ?>
 
 </body>
