bd3714b18a8e89bd22acf33ccd3a1ee882c583ad
Isaacmart <65898932+Isaacmart@users.noreply.github.com>
2021-05-31 04:17:58 -0500

    new

--- webhook.php
+++ webhook.php
@@ -12,7 +12,8 @@
     <?php 
     $new_json = file_get_contents('php://input');
     $data =  json_decode('$new_json');
-    echo "<h2>" . $data . "</h2>";
+    $fp = fopen('webhooks', 'a');
+    fwrite($fp, $data)
     ?>
 
 </body>
