d0c505643660ae8275077ffa7d5f82898af4d497
Isaacmart <65898932+Isaacmart@users.noreply.github.com>
2021-05-31 04:28:05 -0500

    n

--- webhook.php
+++ webhook.php
@@ -9,12 +9,8 @@
     <h1>Receive Webhooks</h1>
 
     <div id="$data"></div>
-    <?php 
-    $new_json = file_get_contents('php://input');
-    $data =  json_decode('$new_json');
-    $fp = fopen('webhooks', 'a');
-    fwrite($fp, $data)
-    ?>
+    <?php echo '<p>Hello World</p>'; ?>
+
 
 </body>
 </html>
