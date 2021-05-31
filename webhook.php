11db2e8b7f881dd35b23561323f15477e52120a1
Isaacmart <65898932+Isaacmart@users.noreply.github.com>
2021-05-31 02:28:14 -0500

    may 31

--- webhook.php
+++ webhook.php
@@ -1,10 +1,22 @@
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
- </body>
+
+<head>
+    <title>Receive Trades</title>
+
+</head>
+
+<body>
+    <h1>Receive Webhooks</h1>
+
+    <div id="webhook"></div>
+
+    <?php 
+    $json = file_get_contents('php://input');
+    $data =  json_decode('$json');
+    echo $data;
+    ?>
+
+</body>
 </html>
 
  
---
+++ webhookListener.js
@@ -0,0 +1,1 @@
+var webhooks = <?php echo json_encode($_POST['my_post'] ?? null) ?>
