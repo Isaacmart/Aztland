8909a95577f9817dbd1c097cb2888a661effa019
Isaacmart <65898932+Isaacmart@users.noreply.github.com>
2021-06-07 14:05:42 -0500

    Catch webhooks in server

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 from flask import Flask, request, abort
-from flup.server.fcgi import WSGIServer
+#from flup.server.fcgi import WSGIServer
 
 
 app = Flask(__name__)
 
+to_write = open("webhooks_log.txt", "a")
 
-@app.route('/webhookListener.py')
-def webhook():
+
+@app.route('/', methods=["POST"])
+def application():
     if request.method == 'POST':
         print(request.json)
-        return 'success', 200
+        to_write.write(request.json + "\n")
+        return 'success!', 200
     else:
         abort(400)
 
 
-if __name__ == '__name__':
-    WSGIServer(app(), bindAddress='/var/www/jdsdkf.xyz/html/CryptoTrader').run()
+#if __name__ == '__name__':
+#    WSGIServer(app(), bindAddress='/var/www/jdsdkf.xyz/html/CryptoTrader').run()
 
 
