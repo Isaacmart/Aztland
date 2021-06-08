4171d5bfb948032d295005dab356f0de072c93ea
Isaacmart <65898932+Isaacmart@users.noreply.github.com>
2021-06-08 02:01:42 -0500

    save webhooklog

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def application():
     if request.method == 'POST':
-        print(request.json)
-        return 'success!', 200, request.json
+        write_to_csv(request.json)
+        return 'success!', 200
     elif request.method == 'GET':
-        return render_template('index.html', webhook=request.json)
+        return render_template('index.html')
     else:
         abort(400)
 
--- webhookListener.py
+++ webhookListener.py
@@ -2,7 +2,7 @@ import csv
 
 
 def write_to_csv(a_string):
-    to_write = open('webhook_log.csv', 'a')
+    to_write = open('webhook_log.csv', 'w')
     writer = csv.writer(to_write)
     writer.writerow(a_string)
     to_write.close()
