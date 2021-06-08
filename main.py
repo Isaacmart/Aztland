0a64a3cb21102e2de8fc5e2ef0fbfc43e360c6ed
Isaacmart <65898932+Isaacmart@users.noreply.github.com>
2021-06-07 19:07:40 -0500

    show webhooks to server

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ from flask import Flask, request, abort
 
 app = Flask(__name__)
 
-to_write = open("webhooks_log.txt", "a")
+#to_write = open("webhooks_log.txt", "a")
 
 
 @app.route('/', methods=["POST"])
 def application():
     if request.method == 'POST':
         print(request.json)
-        to_write.write(request.json + "\n")
-        return 'success!', 200
+        #to_write.write(request.json + "\n")
+        return 'success!', 200, request.json
     else:
         abort(400)
 
