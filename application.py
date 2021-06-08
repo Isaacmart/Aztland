e7ce530dffa8d4db7b05b25e42fb3f6626dff1e5
Isaacmart <65898932+Isaacmart@users.noreply.github.com>
2021-06-08 02:51:23 -0500

    test showing in browser

--- application.py
+++ application.py
@@ -1,14 +1,28 @@
+import cStringIO
+import os
 
 def application(environ, start_response):
-    status = '200 OK'
-    output = b'Hello World!'
+    headers = []
+    headers.append(('Content-Type', 'text/plain'))
+    write = start_response('200 OK', headers)
 
-    response_headers = [('Content-type', 'text/plain'),
-                        ('Content-Length', str(len(output)))]
-    start_response(status, response_headers)
+    input = environ['wsgi.input']
+    output = cStringIO.StringIO()
 
-    return [output]
+    print >> output, "PID: %s" % os.getpid()
+    print >> output, "UID: %s" % os.getuid()
+    print >> output, "GID: %s" % os.getgid()
+    print >> output
 
+    keys = environ.keys()
+    keys.sort()
+    for key in keys:
+        print >> output, '%s: %s' % (key, repr(environ[key]))
+    print >> output
+
+    output.write(input.read(int(environ.get('CONTENT_LENGTH', '0'))))
+
+    return [output.getvalue()]
 
 
 
