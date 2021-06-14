2c6e0090ddf5d6f63c8b01d08d0f5b22cb063bfe
Isaacmart <65898932+Isaacmart@users.noreply.github.com>
2021-06-13 22:39:59 -0400

    fix more print statements

--- application.py
+++ application.py
@@ -14,15 +14,18 @@ def application(environ, start_response):
     print("PID: %s" % os.getpid(), file=output)
     print("UID: %s" % os.getuid(), file=output)
     print("GID; %s" % os.getgid(), file=output)
+    print(file=output)
     #print >> output, "PID: %s" % os.getpid()
     #print >> output, "UID: %s" % os.getuid()
     #print >> output, "GID: %s" % os.getgid()
     #print >> output
 
     keys = environ.keys()
-    keys.sort()
+    #keys.sort()
     for key in keys:
-        print >> output, '%s: %s' % (key, repr(environ[key]))
+        print('%s: %s' % (key, repr(environ[key])), file=output)
+        #print >> output, '%s: %s' % (key, repr(environ[key]))
+    print(file=output)    
     print >> output
 
     output.write(input.read(int(environ.get('CONTENT_LENGTH', '0'))))
