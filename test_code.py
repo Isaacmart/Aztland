41475e8b8d41e5227f1e9034f78c897c22565990
Isaacmart <65898932+Isaacmart@users.noreply.github.com>
2021-07-18 10:28:24 -0400

    debug main

--- test_code.py
+++ test_code.py
@@ -1,12 +1,20 @@
+from cbpro import AuthenticatedClient
+from order import Order
+from open_position import OpenPosition
+import Data
 
+key = Data.API_Public_Key
+b64secret = Data.API_Secret_Key
+passphrase = Data.Passphrase
 
-hist = -0.632370075497763
+client = AuthenticatedClient(key, b64secret, passphrase)
 
-if hist < 0:
-    print(True)
-else:
-    print(False)
-
+new_order = Order(client)
+new_order.get_id()
+new_order.set_details()
+position = OpenPosition(new_order)
+position.set_position()
+print(position.get_position())
 
 
 
