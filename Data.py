70132191a4e268869fe4ea3a0ee3221d56f9cb1d
Isaacmart <65898932+Isaacmart@users.noreply.github.com>
2021-07-20 00:08:23 -0400

    debug main

--- Data.py
+++
@@ -1,5 +0,0 @@
-API_Secret_Key = "e2jzkbYUE/i0aSVY73Rm3Isrwl2IP+xnSN8LkcqLOCY3JRMAOddBKdUMqFm0p48rToJ4PCFPLkwDwR5Wb2r3ew=="
-API_Public_Key = "e04991cc7dc1ac4e3b7f8edc54ed96d4"
-Passphrase = "Hillgrove1!"
-Path = "/var/www/jdsdkf.xyz/html/CryptoTrader/data.txt"
-
