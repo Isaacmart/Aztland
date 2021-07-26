4a91748fde141deed7ebf90337e68c75dbc32b76
Isaacmart <65898932+Isaacmart@users.noreply.github.com>
2021-07-26 00:20:09 -0400

    debug trading rules

--- test_code.py
+++ test_code.py
@@ -1,15 +1,10 @@
-from cbpro import PublicClient
-from indicators import *
+is_falling = False
+is_top = False
 
-client = PublicClient()
+if (is_top and is_falling) is False:
+    print(True)
 
-indicator = MACD(client)
-
-indicator.set_candles(product="ETH-USD", callback=get_time(83929), begin=get_time(0), granularity=900)
-
-for candle in indicator.candles:
-
-    if candle[4] < candle[3]:
-        print(candle)
+else:
+    print(False)
 
 
