155c8a7828fc87473f9b16135a88e28bd35569c0
Isaacmart <65898932+Isaacmart@users.noreply.github.com>
2021-08-02 23:08:44 -0400

    debug main

--- main.py
+++ main.py
@@ -244,10 +244,6 @@ def application():
             # Means that the indicators could not be measured
             pass
 
-        writer = open(Data.New_path + "data/" + new_ticker + ".csv", "a")
-        writer.write(new_ticker + ", " + str(get_time(0)) + "\n")
-        writer.close()
-
         # If there is no a position opened it will trigger a buy order
         if position.get_position() is False:
 
