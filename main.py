58567d74b66dcdd8816ccb3d0bdda3aca1bc7bd5
Isaacmart <65898932+Isaacmart@users.noreply.github.com>
2021-08-11 22:50:16 -0400

    debug main

--- main.py
+++ main.py
@@ -257,13 +257,12 @@ def application():
         abort(400)
 
 
-@app.route("/login", methods=['GET'])
-def request():
-    def login():
-        error = None
-        if request.method == 'POST':
-            if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-                error = 'Invalid Credentials. Please try again.'
-            else:
-                return redirect(url_for('home'))
-        return render_template('login.html', error=error)
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
