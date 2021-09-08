fde22b4ef22252d8563236ba78ae47366cde472e
Isaacmart <65898932+Isaacmart@users.noreply.github.com>
2021-09-08 14:14:52 -0400

    add docstrings

--- indicators.py
+++ indicators.py
@@ -4,13 +4,22 @@ from cbpro import PublicClient
 
 
 class Indicator:
-
-    '''
+    """
     Base class for any indicator.
-    Index 4 will create a list with close price values
-    Gets a initialized PublicClient as parameter
-    '''
+    Coinbase returns data in the following way: [1415398768, 0.32, 4.2, 0.35, 4.2, 12.3],
+    indicators work with close prices as default, therefore we append data from index 4
+    If the indicator is weighted, certain amount of elements back are needed,
+    weight = True will get that many elements automatically
+    """
+
     def __init__(self, index=4, weight=True):
+        """
+        Creates an instance of the Indicator class
+
+        Args:
+             index (int): type of price to work with, close price is default
+             weight (bool): If
+        """
         self.new_client = PublicClient(timeout=45)
         self.candles = []
         self.close_array = []
@@ -20,14 +29,35 @@ class Indicator:
         self.weight = weight
 
     def initiate_client(self, client=PublicClient):
+        """
+        Passes an instance class needed for getting indicators
+
+        Args:
+            client (PublicClient): Instance of PublicClient
+        """
+        self.__doc__ += PublicClient.__doc__
         self.new_client = client
 
     def set_candles(self, product, callback, begin, granularity):
+        """
+        Makes requests to the Coinbase API for Historic rates
+
+        Args:
+            product (str): A valid Coinbase product
+            callback (int):  Number of seconds to go back from begin
+            begin (int): Last second to get rates from
+            granularity (int): Number of seconds
+        """
         self.candles = self.new_client.get_product_historic_rates(product_id=product, start=callback, end=begin, granularity=granularity)
 
         return self.candles
 
     def get_data_set(self):
+        """
+        Gets a data set in a list
+
+        :return: A list with the closing prices of a product
+        """
 
         for candle in self.candles:
             self.close_array.append(float(candle[self.index]))
@@ -35,11 +65,28 @@ class Indicator:
         return self.close_array
 
     def reverse_data(self):
+        """
+        Reverses the list containing the closing price.
+        Coinbase yields the latest data first; however, to
+        get accurate numbers, the data passed to the
+
+        :return: List with closing prices reversed
+        """
 
         self.close_array.reverse()
         return self.close_array
 
     def get_dates(self):
+        """
+        Puts of the dates in a list an reverses it
+        in the same method. That is because when we
+        testing strategies sometimes we wanna see when
+        it happened and we this method we just have to
+        reference the same index as the one in the
+        closing prices list
+
+        :return: Reversed list of dates in Unix form
+        """
 
         p = 0
         while p < len(self.candles):
@@ -51,12 +98,23 @@ class Indicator:
         return self.date_array
 
     def get_np_array(self):
+        """
+        Ta-Lib takes data in a numpy array, this
+        method coverts a regular list into a numpy
+        array
+
+        :return: Numpy array of closing prices
+        """
 
         self.np_array = numpy.array(self.close_array)
         return self.np_array
 
 
 class MACD(Indicator):
+    """
+    Implementation of the MACD indicator, inherits
+    Indicator and
+    """
 
     def __init__(self, fastperiod=12, slowperiod=26, signalperiod=9, index=4, weight=True):
         super(MACD, self).__init__(index=index, weight=True)
--- open_position.py
+++ open_position.py
@@ -1,13 +1,13 @@
 from order import Order
 
 
-#Verifies that there is a long position by getting
-#the last order collected
-#There is a long position as long as an order has been filled
-#and the side was buying
-
-
 class OpenPosition:
+    """
+    Verifies that there is a long position by getting
+    the last order collected
+    There is a long position as long as an order has been filled
+    and the side was buying
+    """
 
     def __init__(self, order=Order()):
         self.long_position = False
