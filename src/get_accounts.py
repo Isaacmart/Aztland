a60efb3f24d60410a662976e0446d611a3204ad4
Isaacmart <65898932+Isaacmart@users.noreply.github.com>
2021-12-08 13:52:03 -0500

    Add functionality to the trader bot

--- src/get_accounts.py
+++
@@ -1,8 +0,0 @@
-from cbpro import AuthenticatedClient
-import Data
-
-private_client = AuthenticatedClient(Data.API_Public_Key, Data.API_Secret_Key, Data.Passphrase)
-for line in private_client.get_accounts():
-    if 'currency' in line:
-        if line['currency'] == "USD":
-            print(line["balance"])
